262f88a203cf62415379929eca2e357c27b62dd2
Hrithwik <hrithwikssukumar@gmail.com>
2025-03-10 19:54:37 +0530

    updated the files

--- sample.js
+++ sample.js
@@ -1,13 +1,68 @@
-document.write("<h1>virat</h1>")
-//this is my first programe
 
-console.log(10+20)
-/*
-rthjk
-rtkrek
-dfdkldf
-rtrthfg
-*/
+//String
+// var person1 = "Virat"
+// let person2 = "Abd"
+
+// //Number
+// var number1 = 10
+// var number2 = 10.45
+
+// //Boolean
+// var js = true
+// var html = false
+
+// //Undefined
+// var sample1;
+// var sample2 = null
+
+
+
+// var person = "Virat"
+// document.write(person + "<br>")
+
+// person = "Rohit"
+
+// document.write(person)
+
+
+
+// const pi = 3.14
+// document.write(pi)
+// pi = 3
+// document.write(pi)
+
+
+// var a = "B" 
+// switch(a){
+//     case "A":
+//         document.write("Apple")
+//         break;
+//     case "B":
+//         document.write("Banana")
+//         break;
+//     case "C":
+//         document.write("Carrot")
+//         break;
+//     default:
+//         document.write("Invalid Character")
+//         break;
+
+// }
+
+
+
+
+// var i = 1;
+// while(i<=5){
+//     document.write(i +"<br>")
+//     i++
+// }
+
+
+
+var a = document.getElementById("head1")
+a.innerHTML = "Python Netwok"
+a.style.color = "red"
 
 
 
